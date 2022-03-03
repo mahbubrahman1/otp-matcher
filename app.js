8798c1e78b797e7bcacb1beea59aa0b6bcfbefb2
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-03-03 16:31:11 +0600

    show pin in display

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("paiche!!!!!");
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinToString = pin + '';
+    if (pinToString.length == 4) {
+        return pin;
+    } else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
